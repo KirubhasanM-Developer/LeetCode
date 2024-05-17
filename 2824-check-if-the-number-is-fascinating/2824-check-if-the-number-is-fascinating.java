ceddca18b61c568e2a8430f0577bca0d3d1926bf
KirubhasanMurugesan <162954926+KirubhasanMurugesan@users.noreply.github.com>
2024-05-17 20:16:59 +0530

    Time: 2 ms (73.74%), Space: 40.7 MB (86.74%) - LeetHub

---
+++ 2824-check-if-the-number-is-fascinating/2824-check-if-the-number-is-fascinating.java
@@ -0,0 +1,33 @@
+class Solution {
+    public boolean isFascinating(int n) {
+        int m=2*n;
+        int l=3*n;
+        String ans=Integer.toString(n);
+        ans=ans+Integer.toString(m);
+        ans=ans+Integer.toString(l);
+        if(ans.length()>9){
+            return false;
+        }
+       
+        boolean b=false;
+        for(int i=1;i<=9;i++){
+            for(int j=0;j<ans.length();j++){
+                int y=ans.charAt(j)-'0';
+                if(y==0){
+                    return false;
+                }
+                if(y==i){
+                    b=true;
+                    break;
+                }else{
+                    b=false;
+                }
+            }
+            if(b==false){
+                return false;
+            }
+        }
+        return b;
+        
+    }
+}
