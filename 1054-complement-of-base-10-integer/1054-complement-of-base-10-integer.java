3ce3e1c950c38adf1690ed8784bbbbc034559b7d
KirubhasanMurugesan <162954926+KirubhasanMurugesan@users.noreply.github.com>
2024-05-21 21:46:05 +0530

    Time: 2 ms (10.23%), Space: 40.3 MB (56.71%) - LeetHub

---
+++ 1054-complement-of-base-10-integer/1054-complement-of-base-10-integer.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int bitwiseComplement(int n) {
+        String ans=Integer.toBinaryString(n);
+        String ans1="";
+        for(int i=0;i<ans.length();i++){
+            if(ans.charAt(i)=='0'){
+                ans1+='1';
+            }else{
+                ans1+='0';
+            }
+        }
+return Integer.parseInt(ans1,2);
+        
+    }
+}
