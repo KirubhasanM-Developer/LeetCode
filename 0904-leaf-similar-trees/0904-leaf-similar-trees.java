683ddc3c9646aa3acdb9652ae586c15727ef39e7
Kirubhasan Murugesan <162954926+KirubhasanM-Developer@users.noreply.github.com>
2024-07-02 12:44:20 +0530

    Time: 0 ms (100%), Space: 41.1 MB (70.38%) - LeetHub

---
+++ 0904-leaf-similar-trees/0904-leaf-similar-trees.java
@@ -0,0 +1,47 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean leafSimilar(TreeNode root1, TreeNode root2) {
+        ArrayList <Integer>li1=new ArrayList<>();
+        ArrayList<Integer>li2=new ArrayList<>();
+        traversal(root1,li1);
+        traversal(root2,li2);
+        if(li1.equals(li2)){
+            return true;
+        }else{
+        return false;}
+    }
+    public static void traversal(TreeNode root,ArrayList<Integer>li){
+        
+        if(root==null){
+            li.add(root.val);
+            return;
+        }
+
+        
+        if (root.left == null && root.right == null) {
+            li.add(root.val);
+        }
+        
+        if (root.left != null) {
+            traversal(root.left, li);
+        }
+        
+        if (root.right != null) {
+            traversal(root.right, li);
+        }
+    }
+}
