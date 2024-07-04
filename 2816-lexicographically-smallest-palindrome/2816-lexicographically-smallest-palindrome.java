da1f752962456d3488d391216d111fda6803785c
Kirubhasan Murugesan <162954926+KirubhasanM-Developer@users.noreply.github.com>
2024-07-04 10:23:34 +0530

    Time: 12 ms (26.87%), Space: 45.1 MB (79.16%) - LeetHub

---
+++ 2816-lexicographically-smallest-palindrome/2816-lexicographically-smallest-palindrome.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String makeSmallestPalindrome(String s) {
+        StringBuilder sb=new StringBuilder(s);
+        int i=0,j=s.length()-1;
+        while(i<j){
+            if(sb.charAt(i)!=sb.charAt(j)){
+                if(sb.charAt(i)<sb.charAt(j)){
+                     sb.setCharAt(j, sb.charAt(i));
+                }else{
+                    sb.setCharAt(i,sb.charAt(j));
+                }
+
+            }
+            i++;
+            j--;
+        }
+        return sb.toString();
+    }
+}
