89b93e9c67a382cb80f63af5f2d96e183cc45b6d
Kirubhasan Murugesan <162954926+KirubhasanM-Developer@users.noreply.github.com>
2024-07-22 23:00:20 +0530

    Time: 24 ms (99.41%), Space: 61.5 MB (63.93%) - LeetHub

--- 2417-the-latest-time-to-catch-a-bus/2417-the-latest-time-to-catch-a-bus.java
+++ 2417-the-latest-time-to-catch-a-bus/2417-the-latest-time-to-catch-a-bus.java
@@ -2,21 +2,22 @@ class Solution {
     public int latestTimeCatchTheBus(int[] buses, int[] passengers, int capacity) {
         Arrays.sort(buses);
         Arrays.sort(passengers);
-        Set<Integer> set = new HashSet<>();
-        int ans=0,j=0;
-        for(int i=0;i<buses.length;i++){
-            int c = 0;
-            while(j<passengers.length && c<capacity && passengers[j]<=buses[i]){
-                if(!set.contains(passengers[j]-1)){
-                    ans=passengers[j]-1;
-                }
-                set.add(passengers[j]);
-                j++; c++;
-            }
-            if(c<capacity && !set.contains(buses[i])){
-                ans = buses[i];
+        int capFilled = 0;
+        int pi=0;
+        for(int bi=0; bi<buses.length; bi++) {
+            capFilled = 0;
+            while(capFilled < capacity && pi<passengers.length && passengers[pi] <= buses[bi]) {
+                capFilled++;
+                pi++;
             }
         }
-        return ans;
+        pi--;
+        if(capFilled < capacity && (pi<0 || buses[buses.length-1] > passengers[pi])) {
+            return buses[buses.length-1];
+        }
+        while(pi>0 && passengers[pi]-1 == passengers[pi-1]) {
+            pi--;
+        }
+        return passengers[pi]-1;
     }
 }
